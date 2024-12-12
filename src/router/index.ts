7e4cd40ffd0ff89ae0737633eea56afaa6bb4107
mattiahit <mattiahit@gmail.com>
2024-12-12 23:55:42 +0100

    fix page import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginPage from '@/views/Pages/Login/LoginPage'
-import ScooterManagementPage from '@/views/Pages/ScooterManagement/ScooterManagementPage'
+import LoginPage from '@/views/Pages/Login/LoginPage.vue'
+import ScooterManagementPage from '@/views/Pages/ScooterManagement/ScooterManagementPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),

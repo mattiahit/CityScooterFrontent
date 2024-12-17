dbe794c62b7f51245566bf754ff0d154272235a4
mattiahit <mattiahit@gmail.com>
2024-12-17 15:53:57 +0100

    aggrid impl

--- src/views/Pages/ScooterManagement/ScooterManagementPage.ts
+++ src/views/Pages/ScooterManagement/ScooterManagementPage.ts
@@ -1,10 +1,12 @@
 import { defineComponent, ref } from "vue";
 import { AgGridVue } from 'ag-grid-vue3'
+import type { GridSizeChangedEvent } from "ag-grid-community";
 
 export default defineComponent({
   name: 'ScooterManagementPage',
   components: { AgGridVue },
   setup() {
+    const gridApi = ref(null);
     const managementRowData = ref([
       { id: 1, name: 'Scooter 1', status: 'Available' },
       { id: 2, name: 'Scooter 2', status: 'In Use' },
@@ -15,9 +17,27 @@ export default defineComponent({
       { headerName: 'Name', field: 'name' },
       { headerName: 'Status', field: 'status' }
     ])
+    const onGridReady = (params) => {
+        gridApi.value = params.api;
+        params.api.sizeColumnsToFit()
+    }
+    const onGridSizeChanged = (params: GridSizeChangedEvent) => {
+      params.api.sizeColumnsToFit();
+    }
+    const managementGridOptions = ref({
+      defaultColDef: {
+        sortable: true,
+        filter: true,
+        resizable: true
+      }
+    })
+    
     return {
       managementRowData,
-      managementColumnDefs
+      managementColumnDefs,
+      managementGridOptions,
+      onGridSizeChanged,
+      onGridReady
     }
   }
 })
--- src/views/Pages/ScooterManagement/ScooterManagementPage.vue
+++ src/views/Pages/ScooterManagement/ScooterManagementPage.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
         <ag-grid-vue
+        class="ag-theme-alpine"
         :rowData="managementRowData"
-        :columnDefs="managementColumnDefs">
+        :columnDefs="managementColumnDefs"
+        :gridOptions="managementGridOptions"
+        @grid-ready="onGridReady"
+        @grid-size-changed="onGridSizeChanged">
         </ag-grid-vue>
     </div>
 </template>
